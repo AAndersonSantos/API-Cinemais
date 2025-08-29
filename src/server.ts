1f33c3d524e145edd31f94af0aee2e5c4f77cddb
AAndersonSantos <andersonsantos.inc@gmail.com>
2025-08-28 22:20:18 -0300

    Chore(docker): configuração do projeto dentro do container

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import Fastify from "fastify";
+import { MongoClient } from "mongodb";
 
 const app = Fastify({ logger: true });
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/cinemais";
+let db: any;
 
 app.get("/", async () => {
   return { message: "Hello Fastify 🚀" };
@@ -8,7 +12,12 @@ app.get("/", async () => {
 
 const start = async () => {
   try {
-    await app.listen({ port: 3000 });
+    const client = new MongoClient(mongoUrl);
+    await client.connect();
+    db = client.db();
+    console.log("Conectado ao MongoDB ✅");
+
+    await app.listen({ port: 3000, host: "0.0.0.0" });
     console.log("Servidor rodando em http://localhost:3000");
   } catch (err) {
     app.log.error(err);
