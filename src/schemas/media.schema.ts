5da45c5ae08360317161dd386215ae8e266857f8
AAndersonSantos <andersonsantos.inc@gmail.com>
2025-08-30 14:26:33 -0300

    FEAT: Atualização do schema

--- src/schemas/media.schema.ts
+++ src/schemas/media.schema.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 
 export const MediaSchema = z.object({
-  title: z.string().min(1, "O título é obrigatório" ),
-  description: z.string().min(1, "A descrição é obrigatória"),
+  title: z.string().nonempty("O título é obrigatório"),
+  description: z.string().nonempty("A descrição é obrigatória"),
   type: z.enum(["movie", "series"], "O tipo deve ser 'movie' ou 'series'"),
   releaseYear: z.number().int().min(1900, "Ano inválido").max(2100, "Ano inválido"),
-  genre: z.string().min(1, "O gênero é obrigatório")
+  genre: z.string().nonempty("O gênero é obrigatório"),
 });
 
 export type MediaInput = z.infer<typeof MediaSchema>;
